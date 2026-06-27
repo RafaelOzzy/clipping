d645517db2325352ede77ad9ac0705e15a3ef0ad
RafaelOzzy <rafael.ramos.s@gmail.com>
2026-06-27 11:36:43 -0300

    Adiciona campos name, cnpj e company ao User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def require_admin!
     unless current_user.admin?
       redirect_to root_path, alert: "Acesso negado!"
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :cnpj, :company])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :cnpj, :company])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_05_173606) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_27_143452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,6 +105,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_05_173606) do
     t.string "role", default: "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "cnpj"
+    t.string "company"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
